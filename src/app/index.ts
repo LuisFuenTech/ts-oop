f4977a094d71a914a68d4e31905f560146e7d54e
luisfuentech <luisfuentes.au@gmail.com>
2024-02-02 17:15:45 -0500

    chore: add singleton to product-http service

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,26 +1,11 @@
-import { faker } from '@faker-js/faker';
-import { ProductMemoryService } from './services/product-memory.service';
 import { ProductHttpService } from './services/product-http.service';
 
-const productService = new ProductMemoryService();
-const productHttpService = new ProductHttpService();
-
-productService.create({
-  title: faker.commerce.productName(),
-  image: faker.image.url(),
-  description: faker.commerce.productDescription(),
-  stock: faker.number.int({ min: 10, max: 100 }),
-  color: faker.color.human(),
-  price: Number(faker.commerce.price({ min: 1000 })),
-  isNew: faker.datatype.boolean(),
-  tags: [faker.lorem.word(), faker.lorem.word()],
-  categoryId: faker.string.uuid(),
-});
+const productHttpService = ProductHttpService.createInstance('service_1');
 
 (async () => {
   try {
     const products = await productHttpService.getAll();
-    const updatedProduct = await productService.update(products[0].id, {
+    const updatedProduct = await productHttpService.update(products[0].id, {
       title: 'Brand new product',
       description: 'This is a new description',
     });
--- src/app/services/product-http.service.ts
+++ src/app/services/product-http.service.ts
@@ -5,8 +5,23 @@ import { IProductService } from '../models/product-service.model';
 import { IProduct } from '../models/product.model';
 
 export class ProductHttpService implements IProductService {
+  static instance: ProductHttpService | null = null;
   private url: string =
     'https://api.escuelajs.co/api/v1/products?limit=25&offset=0';
+
+  constructor(private name: string) {}
+
+  static createInstance(name: string) {
+    if (ProductHttpService.instance === null) {
+      ProductHttpService.instance = new ProductHttpService(name);
+    }
+
+    return ProductHttpService.instance;
+  }
+
+  getName() {
+    return this.name;
+  }
 
   async create(_data: CreateProductDto): Promise<IProduct> {
     const { data } = await axios.post<IProduct>(this.url, _data);
